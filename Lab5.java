eaade6a3f4f8199d779d4c88369ed593527aefed
Johan K <johkin15@gmail.com>
2024-04-29 23:12:24 +0200

    Bytt til bitSet och till byte för hantering av strängsr och besökta

--- Lab5.java
+++ Lab5.java
@@ -2,27 +2,28 @@
 
 import java.util.TreeSet;
 import java.util.Set;
+import java.util.BitSet;
 
 public class Lab5 {
     static int leftAndTopEndOfMartix=0;
     static int rightEndOfMartix;
     static int bottomEndOfMatrix;
     static Kattio io = new Kattio(System.in, System.out);
-    static char[][] matrix; //Hela matrisen
-    static boolean[][] visited; //vilka element som blivit besökta
+    static BitSet visited; //vilka element som blivit besökta
     static Set<Character> charsToPrint = new TreeSet<>(); //Litet innhåll, bara alfabetet, tidkomplexiteten är Log n men praktiskt taget samma som hashset O(1), pluss slipper sortera. 
     static int rowsInMatrix;
     static int collumsInMatrix;
     static char currentChar;
+    static byte[][] matrix;
     public static void main(String[] args) {
         
         rowsInMatrix = io.getInt();
         collumsInMatrix = io.getInt();
         rightEndOfMartix= collumsInMatrix - 1; //för att inte gå utanför 
         bottomEndOfMatrix= rowsInMatrix - 1;
-        matrix = new char[rowsInMatrix][collumsInMatrix];
-        visited = new boolean[rowsInMatrix][collumsInMatrix];
-        MakeMarix(rowsInMatrix, collumsInMatrix);
+        matrix = new byte[rowsInMatrix][collumsInMatrix];
+        visited = new BitSet(rowsInMatrix * collumsInMatrix);
+        MakeMatrix(rowsInMatrix, collumsInMatrix);
         // Utför sökning från varje position i den första raden, förutsatt att tecknet inte redan har en väg bekräftad
         for (int currentColumn = 0; currentColumn < collumsInMatrix; currentColumn++) {
                 depthFirst(0, currentColumn,matrix[leftAndTopEndOfMartix][currentColumn] );
@@ -34,12 +35,14 @@ public class Lab5 {
     /*
      * Tar in måtten på matrisen och klassen för att läsa in matrisen 
      */
-    public static void MakeMarix(int rowsInMatrix, int collumsInMatrix){        
+    static void MakeMatrix(int rowsInMatrix, int columnsInMatrix) {
         for (int i = 0; i < rowsInMatrix; i++) {
             String row = io.getWord();
-            for (int j = 0; j < collumsInMatrix; j++) {
-                matrix[i][j] = row.charAt(j);
-            }}}
+            for (int j = 0; j < columnsInMatrix; j++) {
+                matrix[i][j] = (byte) (row.charAt(j)); 
+            }
+        }
+    }
 
 
             /*
@@ -48,17 +51,18 @@ public class Lab5 {
              * söker rekursivt fram en väg till botten
              * om det redan finns en väg kommer ingen sökning att påbörjas
              */
-    static void depthFirst(int row, int collum, char currentChar) {
+    static void depthFirst(int row, int collum, byte currentChar) {
 
-        if (row < leftAndTopEndOfMartix || collum < leftAndTopEndOfMartix || row >= rowsInMatrix || collum >= collumsInMatrix || visited[row][collum] || charsToPrint.contains(matrix[row][collum]) || 
+        if (row < leftAndTopEndOfMartix || collum < leftAndTopEndOfMartix || row >= rowsInMatrix || collum >= collumsInMatrix || visited.get(row * collumsInMatrix + collum) || charsToPrint.contains((char) matrix[row][collum]) || 
         matrix[row][collum] != currentChar) //Många krav, men i princip bara en koll om man är utanför marisens gränser eller om det är ett besökt element alternativt ett tecken som har väg redan
             return; 
     
-        visited[row][collum] = true;
+            visited.set(row * collumsInMatrix + collum, true);
 
     
         if (row == bottomEndOfMatrix) { //Hittat en väg!
-            charsToPrint.add(currentChar);
+
+            charsToPrint.add((char) currentChar);
             return;
         }
         depthFirst(row, collum+1, currentChar);  // höger
@@ -66,8 +70,6 @@ public class Lab5 {
         depthFirst(row, collum-1, currentChar);  // vänster
         depthFirst(row-1, collum, currentChar);  // Upp
     }
-
-
     /*i princip årtervunnen från tidigasre labb
      * skriver ut alla element i trädet
      * om det inte finns några element skrivs en nolla ut
