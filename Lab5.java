194e12decd2aea7d5e591d6c313b608193600adc
Johan K <johkin15@gmail.com>
2024-04-30 00:10:02 +0200

    Lagt till förklaring

--- Lab5.java
+++ Lab5.java
@@ -11,7 +11,7 @@ public class Lab5 {
     static int bottomEndOfMatrix;
     static Kattio io = new Kattio(System.in, System.out);
     static BitSet visited; //vilka element som blivit besökta
-    static Set<Character> charsToPrint = new TreeSet<>(); //Litet innhåll, bara alfabetet, tidkomplexiteten är Log n men praktiskt taget samma som hashset O(1), pluss slipper sortera. 
+    static Set<Byte> charsToPrint = new TreeSet<>(); //Litet innhåll, bara alfabetet, tidkomplexiteten är Log n men praktiskt taget samma som hashset O(1), pluss slipper sortera. 
     static int rowsInMatrix;
     static int collumsInMatrix;
     static char currentChar;
@@ -27,7 +27,7 @@ public class Lab5 {
         MakeMatrix(rowsInMatrix, collumsInMatrix);
         // Utför sökning från varje position i den första raden, förutsatt att tecknet inte redan har en väg bekräftad
         for (int currentColumn = 0; currentColumn < collumsInMatrix; currentColumn++) {
-                if (!charsToPrint.contains((char) matrix[0][currentColumn])){
+                if (!charsToPrint.contains(matrix[0][currentColumn])){
                 depthFirst(0, currentColumn,matrix[leftAndTopEndOfMartix][currentColumn] );
         }}
         printFromSet(charsToPrint);
@@ -53,44 +53,54 @@ public class Lab5 {
              */
     static void depthFirst(int initialRow, int initialCollum, byte currentChar) {
     
-    Stack<int[]> stack = new Stack<>(); //Skapar en stack, körde tidigare med rekursiv sökning men detta ska flytta belastningen från anropsstacken till heapen vilket kanske löser memory problemet.
+    Stack<int[]> stk = new Stack<>(); //Skapar en stack, körde tidigare med rekursiv sökning men detta ska flytta belastningen från anropsstacken till heapen vilket kanske löser memory problemet.
     //börjar med att lägga startpositionen i stacken, längst ner i loopen kommer allla potentiella vägar läggas till
-    stack.push(new int[] {initialRow, initialCollum});
+    stk.push(new int[] {initialRow, initialCollum});
 
-    while (!stack.isEmpty()) {
+    while (!stk.isEmpty()) {
         // Tar bort och  toppen av stacken, utforskar denna.
-        int[] rowCollum = stack.pop();
-        int row = rowCollum[0];
-        int collum = rowCollum[1];
+        int[] rowCollum = stk.pop(); //Dettta är positionen som ska utforskas
+        int row = rowCollum[0]; //Rad 
+        int collum = rowCollum[1]; //collum 
 
-        
+        //Många krav, men i princip bara en koll om man är utanför marisens gränser eller om det är ett besökt element alternativt ett tecken som har väg redan
         if (row < 0 || row >= rowsInMatrix || collum < 0 || collum >= collumsInMatrix || 
-            visited.get(row * collumsInMatrix + collum) || matrix[row][collum] != currentChar) {//Många krav, men i princip bara en koll om man är utanför marisens gränser eller om det är ett besökt element alternativt ett tecken som har väg redan
-            continue;
+            visited.get(row * collumsInMatrix + collum) || matrix[row][collum] != currentChar || charsToPrint.contains(matrix[row][collum])) {
+                        continue;
         }
 
-        visited.set(row * collumsInMatrix + collum);
-
+        visited.set(row * collumsInMatrix + collum); //För att inte besöka igen, row * collumsInMatrix + collum gör om matrisen till en indexerad lista
+        /*
+         * 
+         *   1 2 3 4 5
+         * 0 0 1 2 3 4
+         * 1 1 2 3 4 5
+         * 2 2 6 .....
+         * 3 3.....
+         * 4 4......
+         * 
+         * 
+         */
        
         if (row == bottomEndOfMatrix) {//Hittat en väg!
-            charsToPrint.add((char) (currentChar));
+            charsToPrint.add((currentChar));
             continue;
         }
-        // Lägger till alla angränsande celler i stacken 
-        stack.push(new int[] {row, collum + 1}); // höger
-        stack.push(new int[] {row + 1, collum}); // ner
-        stack.push(new int[] {row, collum - 1}); // vänster
-        stack.push(new int[] {row - 1, collum}); // upp
+        // Lägger till alla angränsande celler i stacken, först in sist ut, läser matrisen från vänster till höger, och ska neråt, därav dessa i slutet, så de utforskas först
+        stk.push(new int[] {row - 1, collum}); // upp
+        stk.push(new int[] {row, collum - 1}); // vänster
+        stk.push(new int[] {row + 1, collum}); // ner
+        stk.push(new int[] {row, collum + 1}); // höger
     }
 }
     /*i princip årtervunnen från tidigasre labb
      * skriver ut alla element i trädet
      * om det inte finns några element skrivs en nolla ut
      */
-    public static void printFromSet(Set<Character> elements){
+    public static void printFromSet(Set<Byte> elements){
         if (!elements.isEmpty()){
-        for (Character element : elements) {
-            io.print(element);
+        for (byte element : elements) {
+            io.print((char) element);
         } return;}
     else {io.println('0');
 }}}
