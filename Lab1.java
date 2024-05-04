b9b8f3d1541e704d92be417a61bfb81f0e92dff9
Johan K <johkin15@gmail.com>
2024-05-04 11:11:23 +0200

    Ändrat lite inför redovisnign

--- Lab1.java
+++ Lab1.java
@@ -6,39 +6,42 @@ public class Lab1 {
         */
    public static void main(String[] args) {
       
-        Kattio io = new Kattio(System.in, System.out);
-        String string = io.getWord();
-        int length = findLongestLength(string);
-        io.println(length);
-        io.close();
+       Kattio io = new Kattio(System.in, System.out);
+       String string = io.getWord();
+       int length = findLongestLength(string);
+       io.println(length);
+       io.close();
    }
    /*
     * findLongestLength(String string) 
     * input: En sträng
     * räknar längden på alla substrings som är sorterade
-    * lägger dessa längder i en lista
+    * 
     * 
     * return: hur lång den längsta delsträngen är, det högsta värdet i listan
     * 
     */
-    public static int findLongestLength(String string) {
-        if (string == null || string.isEmpty()) return 0; 
-        char[] stringArr = string.toCharArray();
-        int TempLength = 1;
-        int Length = 1; 
+            public static int findLongestLength(String string) {
+      if (string.isEmpty()) return 0; 
+      char[] stringArr = string.toCharArray();
 
-        for (int i = 1; i < string.length(); i++) {
-            if (stringArr[i] >= stringArr[i - 1]) {
-                TempLength++; 
-            } else {
-                if(Length>TempLength){
-                TempLength = 1; 
-            }else{Length = TempLength;
-        TempLength=1;}
-        }}
-       
-       Length = Math.max(Length, TempLength); 
+      int TempLength = 1;
+      int Length = 1; 
 
-       return Length;
-   }
+      for (int i = 1; i < string.length(); i++) {
+          if (stringArr[i] >= stringArr[i - 1]) {
+              TempLength++; 
+          } else {
+              if(Length>TempLength){
+              TempLength = 1; 
+          }else{Length = TempLength;
+               TempLength = 1; 
+
+          }
+      }}
+      
+      Length = Math.max(Length, TempLength); 
+
+      return Length;
+  }
 }
